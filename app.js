680aacca938a2129131eeab8ef4c4b33dd4f115f
1Orchestratooor1 <rodriguezjhon2090@gmail.com>
2025-07-29 22:22:51 -0500

    Se creó la funcion agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 let amigos = [];
+
+function agregarAmigo () {
+    let nombreAmigo = document.getElementById('amigo').value;
+    if (nombreAmigo == "") {
+        alert("El nombre del amigo no puede ser un texto vacio!!!");
+    }else{
+        amigos.push(nombreAmigo);
+        limpiarInput();
+        console.log(amigos)
+    }
+}
+
+function limpiarInput () {
+    document.querySelector("#amigo").value = "";
+}
